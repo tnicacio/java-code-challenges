fb2aef967ae302ead902a5231c903b0bd301d117
tnicacio <nicacio.t92@gmail.com>
2021-05-17 12:29:20 -0300

    evenOrOdd and passwordComplexity solutions

---
+++ src/main/java/javachallenges/solutions/EvenOrOddSolved.java
@@ -0,0 +1,12 @@
+package javachallenges.solutions;
+
+public class EvenOrOddSolved {
+
+	   public static boolean isEven(int n) {
+	       // %
+	       // 4 % 2 = 0
+	       // 3 % 2 = 1
+	       // 6 % 2 = 0
+	       return n % 2 == 0;
+	   }
+}
---
+++ src/main/java/javachallenges/solutions/PasswordComplexitySolved.java
@@ -0,0 +1,43 @@
+package javachallenges.solutions;
+
+public class PasswordComplexitySolved {
+
+	public static boolean isPasswordComplex(String password) {
+		//return solutionWithBuilInMethods(password);
+        return password.length() >= 6 &&
+                password.matches(".*\\d.*") &&
+                password.matches(".*[a-z].*") &&
+                password.matches(".*[A-Z].*");
+	}
+	
+	public static boolean solutionWithBuilInMethods(String password) {
+
+		int passwordLength = password.length();
+		if (passwordLength < 6) {
+			return false;
+		}
+
+		boolean hasLowercaseLetter = false;
+		boolean hasUppercaseLetter = false;
+		boolean hasNumber = false;
+
+
+		for (int i = 0; i < passwordLength || 
+				(!hasLowercaseLetter
+						&& !hasUppercaseLetter
+						&& !hasNumber); i++) {
+
+			char current = password.charAt(i);
+
+			if (Character.isDigit(current)) {
+				hasNumber = true;
+			} else if (Character.isUpperCase(current)) {
+				hasUppercaseLetter = true;
+			} else if (Character.isLowerCase(current)) {
+				hasLowercaseLetter = true;
+			}
+		}
+
+		return hasNumber && hasUppercaseLetter && hasLowercaseLetter;
+	}
+}
